2d5410e1d03ab53e5438dc5d6065dffd47e27ab3
idmitrymolchanov <55110624+idmitrymolchanov@users.noreply.github.com>
2020-01-15 11:38:52 +0300

    delete old version

--- psychotestJNDI/src/main/java/psychotest/repository/Database2Repo.java
+++
@@ -1,9 +0,0 @@
-package psychotest.repository;
-
-import psychotest.database2.entity.SberTest2;
-
-import java.util.List;
-
-public interface Database2Repo {
-    public void saveBatch(final List<SberTest2> employeeList);
-}
